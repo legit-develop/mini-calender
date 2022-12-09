1f8e9f1e05fde8e7f475d929acee29bf3144f31a
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-12-09 22:31:20 +0200

    declare day number element

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const monthName = document.getElementById("month-name");
 console.log(monthName);
+const dayName = document.getElementById("day-name");
+const dayNum = document.getElementById("day-number");
